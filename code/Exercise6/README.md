853cf0113330fb44e37f8202b47a9fc3ef218b69
mfouillen <mathis.fouillen@etudiant.univ-rennes.fr>
2023-12-04 09:31:21 +0100

    Reporter prints only asked data

--- code/Exercise6/README.md
+++ code/Exercise6/README.md
@@ -2,4 +2,4 @@
 
 The code of the exercise is avaible [here](../javaparser-starter/src/main/java/fr/istic/vv/TCCReporter.java).
 
-To use test files, launch fr.istic.vv.Main with following args : "code/javaparser-starter/src/main/java/fr/istic/vv/Exercise3/"
+To use test files, launch fr.istic.vv.Main with following args : "code/javaparser-starter/src/main/java/fr/istic/vv/Exercise3/ TCC"
--- code/javaparser-starter/src/main/java/fr/istic/vv/TCCReporter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/TCCReporter.java
@@ -5,27 +5,89 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 import com.github.javaparser.utils.Pair;
 
-public class TCCReporter extends FieldReporter {
+public class TCCReporter extends VoidVisitorWithDefaults<Void>{
 
+    private final String TAB = "  ";
+
+    private List<String> variables;
     private Map<String, List<String>> methods;
+    
 
-    @Override
-    protected void init(){
-        super.init();
+    private void init(){
+        System.out.println();
+        variables = new ArrayList<>();
         methods = new HashMap<>();
     }
 
+     /************* PARSE SECTION *************/
+
+    // file
+    @Override
+    public void visit(CompilationUnit unit, Void arg) {
+        init();
+        for(TypeDeclaration<?> type : unit.getTypes()) {
+            type.accept(this, null);
+        }
+        writeReport();
+    }
+
+    // class or interface
+    @Override
+    public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
+        visitTypeDeclaration(declaration, arg);
+    }
+
+    // enum
+    @Override
+    public void visit(EnumDeclaration declaration, Void arg) {
+        visitTypeDeclaration(declaration, arg);
+    }
+
+
+    // class
+    public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
+        System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
+
+        for(FieldDeclaration field : declaration.getFields()) {
+            field.accept(this, arg);
+        }
+
+        for(MethodDeclaration method : declaration.getMethods()) {
+            method.accept(this, arg);
+        }
+        
+    }
+
+    // field
+    @Override
+    public void visit(FieldDeclaration declaration, Void arg){
+        for(VariableDeclarator variableDeclarator : declaration.getVariables()){
+            // add the variable to the list of variables
+            variables.add(variableDeclarator.getNameAsString());
+        }
+    }
+
+    // method
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
         
         // Find used variables;
         List<String> usedVariables = new ArrayList<>();
-        for(String word : declaration.getBody().get().toString().split(" |;|this.|(|)")){
-            if(!usedVariables.contains(word) && variables.contains(word)){
-                usedVariables.add(word);
+        if(declaration.getBody().isPresent()){
+            for(String word : declaration.getBody().get().toString().split(" |;|this.|(|)")){
+                if(!usedVariables.contains(word) && variables.contains(word)){
+                    usedVariables.add(word);
+                }
             }
         }
         methods.put(declaration.getNameAsString(), usedVariables);
@@ -33,14 +95,16 @@ public class TCCReporter extends FieldReporter {
         super.visit(declaration, arg);
     }
 
-    private void writeTCCReport() {
-        System.out.println("\n" + TAB + "------TCC REPORT------");
+    /************* REPORT SECTION *************/
 
-        System.out.println(TAB + "Methods : " + methods);
+    private void writeReport() {
+        System.out.println("\n" + TAB + "------REPORT------");
+
+        // System.out.println(TAB + "Methods : " + methods);
 
         Map<Pair<String, String>, List<String>> cohesionMap = getCohesionMap();
 
-        System.out.println(TAB + "Cohesion Map : " + cohesionMap);
+        // System.out.println(TAB + "Cohesion Map : " + cohesionMap);
 
         long numberOfEdges = 0;
 
@@ -74,12 +138,6 @@ public class TCCReporter extends FieldReporter {
             }
         }
         return cohesionMap;
-    }
-
-    @Override
-    protected void writeReport(){
-        super.writeReport();
-        writeTCCReport();
     }
     
 }
